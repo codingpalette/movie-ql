f8c1d9e2e7c51df9b36c774e85d4836dcb28728e
codingpalette <codingpalette@gmail.com>
2020-04-24 23:11:57 +0900

    GraphQL서버 만들기 #12

--- back/graphql/db.js
+++ back/graphql/db.js
@@ -1,55 +1,16 @@
-let movies = [
-  {
-    id: 1,
-    name: '스타워즈',
-    score: 1,
-  },
-  {
-    id: 2,
-    name: '라라랜드',
-    score: 8,
-  },
-  {
-    id: 3,
-    name: '어벤져스',
-    score: 7,
-  },
-  {
-    id: 4,
-    name: '데스노트',
-    score: 5,
-  },
-  {
-    id: 5,
-    name: '조커',
-    score: 8,
-  },
-];
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.am/api/v2/list_movies.json?';
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  // console.log(filteredMovies[0]);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: parseInt(`${movies.length + 1}`),
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
